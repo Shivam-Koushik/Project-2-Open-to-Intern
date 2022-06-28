b1c162cc55235493f70b4368ef7f9a8b8d5014ad
shivamd-96 <shivamd96@outlook.com>
2022-06-28 16:48:30 +0530

    model

--- src/models/collegeModel.js
+++ src/models/collegeModel.js
@@ -1,17 +1,12 @@
 const mongoose = require('mongoose')
 
 const collegeSchema = new mongoose.Schema({
-        name: { type: String, required: true },
-        lname: { type: String, required: true },
-        title: {
-                type: String, required: true,
-                enum: ["Mr", "Mrs", "Miss"]
-        },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
+        name: { type: String, required: true, unique: true },
+        fulllname: { type: String, required: true },
+        logoLink: { type: String, required: true },
         isDeleted: { type: Boolean, default: false }
 
 
 }, { timestamps: true });
 
-module.exports = mongoose.model
+module.exports = mongoose.model("College", collegeSchema)
