08b8b96132d157c1d87117433287754d96b0ed06
Shivam-Koushik <93977658+ShivamKoushik@users.noreply.github.com>
2022-06-28 16:51:18 +0530

    1

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-
-
-mongoose.connect("mongodb+srv://radon-AnandSharma:Anand570@cluster0.riu1k.mongodb.net/group18Database?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ShivamKoushik:s%40H9663334444@cluster0.k1qkf.mongodb.net/Shivam4545-DB2?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -19,5 +17,3 @@ app.use('/', route);
 app.listen( 3000, function () {
     console.log('Express app running on port ' +  3000)
 });
-
-
